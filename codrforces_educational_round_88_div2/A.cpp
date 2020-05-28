4d8405e4e0af3d85d2db54558cdf70eee8b463dd
sharma-raghav <raghav_sharma@outlook.in>
2020-05-28 22:33:08 +0530

    Problem a and b

--- codrforces_educational_round_88_div2/A.cpp
+++ codrforces_educational_round_88_div2/A.cpp
@@ -2,5 +2,30 @@
 #define ll long long
 using namespace std;
 int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        if (m == 0) {
+            cout << 0 << endl;
+            continue;
+        } else {
+            int eachp = n / k;
+            if (eachp >= m) {
+                cout << m << endl;
+                continue;
+            } else {
+                int leftjokers = m - eachp;
+                if (leftjokers%(k-1) == 0) {
+                    cout << eachp - leftjokers / (k - 1) << endl;
+                }
+                else
+                {
+                    cout << eachp - leftjokers / (k - 1) - 1 << endl;
+                }
+            }
+        }
+    }
     return 0;
 }
--- codrforces_educational_round_88_div2/B.cpp
+++ codrforces_educational_round_88_div2/B.cpp
@@ -1,6 +1,58 @@
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
+ll sum(vector<char> row, int x, int y) {
+    ll ans = 0;
+    for (int i = 0; i < row.size(); i++) {
+        if (row[i] == '.' && i+1 < row.size() && row[i+1] == '.') {
+            if (y < 2*x) {
+                ans += y;
+                i++;
+            }
+            else
+            {
+                ans += 2 * x;
+                i++;
+            }
+        } else if (row[i] == '.') {
+            ans = ans + x;
+        }
+    }
+    return ans;
+}
 int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, m, x, y;
+        cin >> n >> m >> x >> y;
+        vector<vector<char>> pave(n, vector<char>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> pave[i][j];
+            }
+        }
+        ll ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans += sum(pave[i], x, y);
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
+/*
+4
+1 1 10 1
+.
+1 2 10 1
+..
+2 1 10 1
+.
+.
+3 3 3 7
+..*
+*..
+.*.
+
+*/
